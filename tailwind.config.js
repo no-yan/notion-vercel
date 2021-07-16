2c9553dfca1aff0041c8ad8a0a70e10cbcd3a316
noyan <1.2.3.up.top.charm00ok001996991@gmail.com>
2021-07-16 18:35:17 +0900

    enable purge for tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  mode: 'jit',
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
     extend: {},
